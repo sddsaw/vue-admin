3846680276b2b0bb43f34f59f96a6649c7f6f0a2
周恩波 <1240397337@qq.com>
2022-07-30 23:12:43 +0800

    feat: 添加.eslintrc.cjs globals规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,11 @@
 module.exports = {
+  // TODO 这些都可以不引用就可以使用的，但是eslint规则校验不过，so 自定义
+  // globals: {
+  //   defineEmits: 'readonly',
+  //   defineExpose: 'readonly',
+  //   defineProps: 'readonly',
+  //   withDefaults: 'readonly'
+  // },
   env: {
     browser: true,
     es2021: true
