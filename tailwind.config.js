3ff03a85afaea2b22b66e9a4ab007f0bd45216db
周恩波 <1240397337@qq.com>
2022-08-02 13:36:37 +0800

    docs: upload tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {}
   },
